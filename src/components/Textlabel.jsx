82e59bae7e023ba85380440462a067198616396f
Pardum <anonymosek@gmail.com>
2023-10-21 00:53:40 +0530

    Adding Comment component

---
+++ src/components/Textlabel.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Textlabel(){
+    return(
+        <div class="mb-3">
+  <label for="exampleFormControlTextarea1" class="form-label"></label>
+  <textarea class="form-control" rows="3"></textarea>
+ </div>
+    );
+}
+
